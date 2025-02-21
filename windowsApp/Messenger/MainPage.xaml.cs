cc5e54c0d92957424f1ca4bcca3bae48f0edfb6f
Sarvar Mumtozbekov <mumtozbekov@gmail.com>
2025-02-21 17:41:52 +0500

    impost dll

--- windowsApp/Messenger/MainPage.xaml.cs
+++ windowsApp/Messenger/MainPage.xaml.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+
+using TCP;
+
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
+using static TCP.NativeBindings;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
@@ -25,6 +31,26 @@ namespace Messenger
         public MainPage()
         {
             this.InitializeComponent();
+            IntPtr symbolsPtr = NativeBindings.shared_symbols();
+            var symbols = Marshal.PtrToStructure<NativeBindings.shared_ExportedSymbols>(symbolsPtr);
+
+
+            var getPlatformNamePtr = symbols.kotlin.root.uz.uniconsoft.messenger.Platform.get_name;
+            var getPlatformName = Marshal.GetDelegateForFunctionPointer<GetPlatformNameDelegate>(getPlatformNamePtr);
+            IntPtr platformNamePtr = getPlatformName();
+            string platformName = Marshal.PtrToStringAnsi(platformNamePtr);
+            Console.WriteLine($"Platform Name: {platformName}");
+
+            // Call Greeting
+            var createGreetingPtr = symbols.kotlin.root.uz.uniconsoft.messenger.Greeting.Greeting;
+            var createGreeting = Marshal.GetDelegateForFunctionPointer<CreateGreetingDelegate>(createGreetingPtr);
+            IntPtr greetingInstancePtr = createGreeting();
+
+            var greetPtr = symbols.kotlin.root.uz.uniconsoft.messenger.Greeting.greet;
+            var greet = Marshal.GetDelegateForFunctionPointer<GreetDelegate>(greetPtr);
+            IntPtr greetingMessagePtr = greet(greetingInstancePtr);
+            string greetingMessage = Marshal.PtrToStringAnsi(greetingMessagePtr);
+            Console.WriteLine($"Greeting: {greetingMessage}");
         }
     }
 }
---
+++ windowsApp/Messenger/NativeBindings.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    class NativeBindings
+    {
+        private const string DllName = "shared.dll"; // Replace with your DLL name without the extension
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr shared_symbols();
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct shared_ExportedSymbols
+        {
+            public IntPtr DisposeStablePointer;
+            public IntPtr DisposeString;
+            public IntPtr IsInstance;
+            // Add other members as needed...
+
+            public KotlinFunctions kotlin;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KotlinFunctions
+        {
+            public Root root;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Root
+        {
+            public Uz uz;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Uz
+        {
+            public Uniconsoft uniconsoft;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Uniconsoft
+        {
+            public Messenger messenger;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Messenger
+        {
+            public PlatformFunctions Platform;
+            public GreetingFunctions Greeting;
+            public WindowsPlatformFunctions WindowsPlatform;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PlatformFunctions
+        {
+            public IntPtr _type;
+            public IntPtr get_name;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct GreetingFunctions
+        {
+            public IntPtr _type;
+            public IntPtr Greeting;
+            public IntPtr greet;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WindowsPlatformFunctions
+        {
+            public IntPtr _type;
+            public IntPtr WindowsPlatform;
+            public IntPtr get_name;
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate IntPtr GetPlatformNameDelegate();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate IntPtr CreateGreetingDelegate();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate IntPtr GreetDelegate(IntPtr greetingInstance);
+    }
+
+}
